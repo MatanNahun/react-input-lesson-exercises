65948c6a38c6758715b4a326d75a13cc98da5335
MatanNahum <matan.nahum95@gmail.com>
2022-11-07 17:24:10 +0200

    input ex done

--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -1,23 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Exercise2 extends Component {
-
-constructor() {
-    super()
+  constructor() {
+    super();
     this.state = {
-        name: "",
-        fruit: ""
-    }
-}
+      name: "",
+      fruit: "",
+    };
+  }
 
-render() {
-    return (
-        <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
-        </div>
+  fruitHandler = (event) => {
+    this.setState({ fruit: event.target.value }, () =>
+      console.log(`${this.state.name} chose the ${this.state.fruit}`)
     );
-}
+  };
+
+  nameHandler = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          name="name"
+          value={this.state.name}
+          onChange={this.nameHandler}
+          id="name-input"
+        />
+        <select
+          id="select-input"
+          value={this.state.fruit}
+          onChange={this.fruitHandler}
+        >
+          <option hidden>Select Fruit...</option>
+          <option value="banana">banana</option>
+          <option value="grape">grape</option>
+          <option value="malon">malon</option>
+          <option value="Watermelon">Watermelon</option>
+        </select>
+      </div>
+    );
+  }
 }
 
 export default Exercise2;
