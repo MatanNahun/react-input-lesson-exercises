37e8c4b5569a56bb4ecbb7e636019edb31ddb6da
MatanNahum <matan.nahum95@gmail.com>
2022-11-07 16:45:49 +0200

    ex1 done

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -1,24 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Exercise1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      age: "",
+    };
+  }
 
-    constructor() {
-        super()
-        this.state = {
-            name: "",
-            age: ""
-        }
-    }
+  changeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
-            </div>
-        );
-    }
+  changeAge = (event) => {
+    this.setState({
+      age: event.target.value,
+    });
+  };
+
+  aleetInfo = () => {
+    alert(`Come in ${this.state.name} you're ${this.state.age}`);
+  };
+
+  //   handleInputChange = e => {
+  //     const value = e.target.value;
+  //     const inputName = e.target.name;
+  //     this.setState({
+  //       [inputName]: value,
+  //     });
+  //   };
+
+  render() {
+    return (
+      <div>
+        <input
+          id="name-input"
+          value={this.state.name}
+          onChange={this.changeName}
+        />
+        <input
+          id="age-input"
+          value={this.state.age}
+          onChange={this.changeAge}
+        />
+        <button onClick={this.aleetInfo}>Go to Bar</button>
+      </div>
+    );
+  }
 }
 
 export default Exercise1;
